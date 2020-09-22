d9a450563f7c2e15b145f5282aaa66a012b12ea7
pouple <antonin.verdier@laposte.net>
2020-09-22 15:45:46 +0200

    new spectro function

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os 
+import scipy
+import librosa
+from librosa import load, display
 import numpy as np 
 import matplotlib.pyplot as plt 
 
@@ -12,15 +15,74 @@ params = sett.parameters()
 
 #Extract data
 data_path = '../Samples/'
-samplerate, sample = wavfile.read(os.path.join(data_path, 'example01.wav'))
+sample, samplerate = librosa.load(os.path.join(data_path, 'example02.wav'),
+								  sr=None, mono=True, offset=0.0, duration=None)
 
-# Remove stereo
-sample = np.transpose(sample[:, 0])
+print(sample.strides[0])
+
+print(len(sample))
+# plt.figure()
+# display.waveplot(y=sample, sr=samplerate)
+# plt.xlabel('Time (sec)')
+# plt.ylabel('Amplitude')
+# plt.show()
+# Remove stereoif needed
+# sample = np.transpose(sample[:, 0])
+
+def fast_fourier(sample, samplerate, plot=False):
+	fft = scipy.fft(sample)
+	if plot:
+		n = len(sample)
+		T = 1/samplerate
+		xf = np.linspace(0, 1/(2*T), n//2)
+		plt.plot(xf, 2/n * np.abs(fft[:n//2]))
+		plt.xlabel('Frequency')
+		plt.ylabel('Magnitude')
+		plt.show()
+
+	return fft
+
+def spectrogram(sample, samplerate, window_ms=20, stride_ms=10, max_freq=4000, eps=1e-14):
+	window_size = int(window_ms * samplerate * 0.001)
+	stride_size = int(stride_ms * samplerate * 0.001)
+
+	# Truncate signal
+	truncate_size = (len(sample) - window_size) % stride_size
+	sample = sample[:len(sample) - truncate_size]
+
+	nshape = (window_size, (len(sample) - window_size) // stride_size + 1)
+	nstrides = (sample.strides[0], sample.strides[0] * stride_size)
+
+	windows = np.lib.stride_tricks.as_strided(sample, shape=nshape, strides=nstrides)
+
+	assert np.all(windows[:, 1] == sample[stride_size:(stride_size + window_size)])
+
+	# Window weighting, square fft, scaling
+	weighting = np.hanning(window_size)[:, None]
+
+	fft = np.fft.rfft(windows * weighting, axis=0)
+	fft = np.absolute(fft)
+	fft = fft**2
+
+	scale = np.sum(weighting**2) * samplerate
+	fft[1:-1, :] *= (2.0 / scale)
+	fft[(0, -1), :] /= scale 
+
+	freqs = samplerate / window_size * np.arange(fft.shape[0])
+
+	ind = np.where(freqs <= max_freq)[0][-1] + 1
+	specgram = np.log(fft[:ind, :] + eps)
+
+	return specgram
+
+
 
 
 def spectro(sample, samplerate, plot=False):
 	frequencies, times, spectrogram = signal.spectrogram(sample, samplerate)
 
+	# Perform min-max normalization
+	spectrogram = (spectrogram - np.min(spectrogram))/(np.max(spectrogram) - np.min(spectrogram))
 
 	if plot:
 		# To save in desired location
@@ -51,10 +113,23 @@ def create_amplitude_mask(res, random=False):
 	return amplitude_mask
 
 
+specgram = spectrogram(sample, samplerate)
+plt.pcolormesh(specgram)
+plt.show()
+
 frequencies, times, spectrogram = spectro(sample, samplerate, plot=False)
+print(spectrogram.shape)
+
 
 spatial_tensor = create_spatial_masks(params.size_implant, params.freq_resolution, random=True)
 amplitude_mask = create_amplitude_mask(params.freq_resolution, random=True)
+
+# Extract frequencies for a given time
+freq_series = [spectrogram[:, i] for i in range(spectrogram.shape[0])]
+
+# Filter them by 2D mask
+
+# Apply frequency selectivity
 
 
 
