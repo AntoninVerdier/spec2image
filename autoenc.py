d882726f0cefb61d6662890c2e092c5492866f50
pouple <antonin.verdier@laposte.net>
2021-03-04 19:34:38 +0100

    add matplotlib

--- autoenc.py
+++ autoenc.py
@@ -2,6 +2,9 @@ import keras
 import numpy as np
 from keras import layers
 from keras.datasets import mnist
+
+import matplotlib.pyplot as plt
+
 # This is the size of our encoded representations
 encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
 
@@ -19,6 +22,7 @@ encoder = keras.Model(input_img, encoded)
 
 encoded_input = keras.Input(shape=(encoding_dim,))
 decoder_layer = autoencoder.layers[-1]
+decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
 
 autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 
@@ -34,3 +38,24 @@ autoencoder.fit(X_train, X_train,
                 batch_size=256,
                 shuffle=True,
                 validation_data=(X_test, X_test))
+
+encoded_imgs = encoder.predict(X_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+n = 10  # How many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # Display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(X_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # Display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
